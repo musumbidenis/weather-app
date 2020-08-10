41557ab9df6d77f2185794d56a0cd7a43a7ca352
Musumbi Denis <musumbidenis@gmail.com>
2020-08-10 13:21:24 +0300

    Added fetchWeather method

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" placeholder="Search.." class="search-bar">
+        <input 
+          type="text"
+          class="search-bar" 
+          placeholder="Search.."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
       </div>
 
       <div class="weather-wrap">
@@ -27,9 +33,35 @@ export default {
     return{
       api_key: "cb7db5fe34f9eb92a11870c8082cc5d5",
       url_base: "api.openweathermap.org/data/2.5/",
+      query:'',
+      weather:{}
     }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    // dateBuilder () {
+    //   let d = new Date();
+    //   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    //   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    //   let day = days[d.getDay()];
+    //   let date = d.getDate();
+    //   let month = months[d.getMonth()];
+    //   let year = d.getFullYear();
+    //   return `${day} ${date} ${month} ${year}`;
+    // }
   }
 }
+
 </script>
 
 <style>
